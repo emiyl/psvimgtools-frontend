19cc74049e2c93d73acd213260d21f25069f639e
Silica <earsyum@gmail.com>
2017-03-01 19:55:54 +1300

    Update For SFO Parsing

--- defs.py
+++ defs.py
@@ -1,14 +1,13 @@
+
 import os
 import platform
 import subprocess
 import ConfigParser
 import sys
-
 from os.path import expanduser
 
-
 def getCmaDir():
-    text = text_file = open("cmadir.txt", "r")
+    text = text_file = open('cmadir.txt', 'r')
     a = text.read()
     text_file.close()
     return a
@@ -16,115 +15,119 @@ def getCmaDir():
 
 def getKey():
     import os
-    html_file = open("tempKey.html")
+    html_file = open('tempKey.html')
     line = html_file.read()
     line = line.splitlines()[16]
     line = line[25:]
     html_file.close()
-    os.remove("tempKey.html")
+    os.remove('tempKey.html')
     return line
+
 
 def isPlugin(path):
     import os
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    if os.path.exists(dir_path+"/easyinstallers/"+path+"/main.py"):
+    if os.path.exists(dir_path + '/easyinstallers/' + path + '/main.py'):
         return True
+
 
 def getWorkingDir():
     return os.path.dirname(os.path.realpath(__file__))
 
+
 def getAid(account):
-    aid = open("accounts/" + account, 'r')
+    aid = open('accounts/' + account, 'r')
     CmaAID = aid.read()
     return CmaAID
 
+
 def getStoredKey(account):
-    key = open("keys/" + account, 'r')
+    key = open('keys/' + account, 'r')
     CmaKey = key.read()
     return CmaKey
 
-def isBackup(dir):
-    if os.path.isfile(dir+".psvinf"):
 
+def isBackup(dir):
+    if os.path.isfile(dir + '.psvinf'):
         return True
+
 
 def isEncryptedApp(dir):
-    if os.path.isfile(dir+"/license/license.psvmd"):
+    if os.path.isfile(dir + '/license/license.psvmd'):
         return True
+
 
 def isApp(dir):
-    if os.path.isfile(dir+"/license.psvmd-dec"):
+    if os.path.isfile(dir + '/license.psvmd-dec'):
         return True
 
+
 def autoCMA():
-    if sys.platform.__contains__("linux"):
-        home = expanduser("~")
+    if sys.platform.__contains__('linux'):
+        home = expanduser('~')
         configParser = ConfigParser.RawConfigParser()
-        configFilePath = home+"/.config/codestation/qcma.conf"
+        configFilePath = home + '/.config/codestation/qcma.conf'
         configParser.read(configFilePath)
         line = configParser.get('General', 'appsPath')
-
-        text_file = open("cmadir.txt", "w+")
+        text_file = open('cmadir.txt', 'w+')
         text_file.write(line)
         text_file.close()
-        print "CMA Dir: " + line
-
-    if sys.platform.__contains__("win"):
+        print 'CMA Dir: ' + line
+    if sys.platform.__contains__('win'):
         import _winreg
-        qcma = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, "Software\codestation\qcma")
-        path = _winreg.QueryValueEx(qcma, "appsPath")
-        print "CMA Dir: " + path[0]
-        text_file = open("cmadir.txt", "w+")
+        qcma = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, 'Software\\codestation\\qcma')
+        path = _winreg.QueryValueEx(qcma, 'appsPath')
+        print 'CMA Dir: ' + path[0]
+        text_file = open('cmadir.txt', 'w+')
         text_file.write(path[0])
         text_file.close()
         _winreg.CloseKey(qcma)
 
 
 def autoAccount():
-
-    ##GET AID FROM QCMA CONFIG
-    if sys.platform.__contains__("linux"):
-        home = expanduser("~")
+    if sys.platform.__contains__('linux'):
+        home = expanduser('~')
         configParser = ConfigParser.RawConfigParser()
-        configFilePath = home+"/.config/codestation/qcma.conf"
+        configFilePath = home + '/.config/codestation/qcma.conf'
         configParser.read(configFilePath)
         aid = configParser.get('General', 'lastAccountId')
-        print "AID: " + aid
-    if sys.platform.__contains__("win"):
+        print 'AID: ' + aid
+    if sys.platform.__contains__('win'):
         import _winreg
-        qcma = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, "Software\codestation\qcma")
-        aid = _winreg.QueryValueEx(qcma, "lastAccountId")
+        qcma = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, 'Software\\codestation\\qcma')
+        aid = _winreg.QueryValueEx(qcma, 'lastAccountId')
         aid = aid[0]
-        print "AID: " + aid
+        print 'AID: ' + aid
         _winreg.CloseKey(qcma)
-    ##GET ACCOUNT NAME FROM QCMA CONFIG
-    if sys.platform.__contains__("linux"):
-        home = expanduser("~")
+    if sys.platform.__contains__('linux'):
+        home = expanduser('~')
         configParser = ConfigParser.RawConfigParser()
-        configFilePath = home+"/.config/codestation/qcma.conf"
+        configFilePath = home + '/.config/codestation/qcma.conf'
         configParser.read(configFilePath)
         acc = configParser.get('General', 'lastOnlineId')
-
-        print "Account Name: " + acc
-    if sys.platform.__contains__("win"):
+        print 'Account Name: ' + acc
+    if sys.platform.__contains__('win'):
         import _winreg
-        qcma = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, "Software\codestation\qcma")
-        acc = _winreg.QueryValueEx(qcma, "lastOnlineId")
+        qcma = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER, 'Software\\codestation\\qcma')
+        acc = _winreg.QueryValueEx(qcma, 'lastOnlineId')
         acc = acc[0]
-        print "Account Name: " + acc
+        print 'Account Name: ' + acc
         _winreg.CloseKey(qcma)
-
-
     import urllib
-    print "Downloading Key From: " + "http://cma.henkaku.xyz/?aid=" + aid
-    urllib.urlretrieve("http://cma.henkaku.xyz/?aid=" + aid, "tempKey.html")
+    print 'Downloading Key From: ' + 'http://cma.henkaku.xyz/?aid=' + aid
+    urllib.urlretrieve('http://cma.henkaku.xyz/?aid=' + aid, 'tempKey.html')
     key = getKey()
-    print "CMA Key: "+ key
-
-    text_file = open("keys/" + acc, "w+")
+    print 'CMA Key: ' + key
+    text_file = open('keys/' + acc, 'w+')
     text_file.write(key)
     text_file.close()
-
-    text_file = open("accounts/" + acc, "w+")
+    text_file = open('accounts/' + acc, 'w+')
     text_file.write(aid)
     text_file.close()
+
+
+def getTitleID(backup):
+    output = backup[backup.find('(') + 1:]
+    output = output[:-1]
+    return output
+
