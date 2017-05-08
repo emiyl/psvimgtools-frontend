718e93650cff28cedc13fb13999d97fb565a01c4
Silica <earsyum@gmail.com>
2017-05-08 20:36:47 +1200

    V0.3.2!

--- easyinstallers/CmBackup/installCmBackup.py
+++ easyinstallers/CmBackup/installCmBackup.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import tkFileDialog
 import tkMessageBox
 import zipfile
@@ -35,6 +36,11 @@ def cmbackupextract(account,file=""):
     defs.extractZip(src=cmfile, dst=defs.getCmaDir() + '/EXTRACTED/' + loadtype + "/" + CMABACKUP)
     os.remove(defs.getCmaDir() + '/EXTRACTED/' + loadtype + "/" + CMABACKUP + "/" + "load.txt")
     os.remove(defs.getCmaDir() + '/EXTRACTED/' + loadtype + "/" + CMABACKUP + "/" + "TitleID.txt")
+    if os.path.exists(defs.getCmaDir() + '/EXTRACTED/' + loadtype + "/" + CMABACKUP + '/savedata') == True:
+        savedata = tkMessageBox.askyesno(title="Savedata",message="This .cmbackup contains savedata, would you like to use this savedata?\nYou may have problems with trophy eligiby if you do.")
+        if savedata == False:
+            shutil.rmtree(defs.getCmaDir() + '/EXTRACTED/' + loadtype + "/" + CMABACKUP + '/savedata')
+            os.remove(defs.getCmaDir() + '/EXTRACTED/' + loadtype + "/" + CMABACKUP + '/savedata.psvmd-dec')
     sign_support.goSign(account,loadtype,CMABACKUP,True)
     tkMessageBox.showinfo(title="Done!",message="Done!, Backup Created.")
     if file != "":
