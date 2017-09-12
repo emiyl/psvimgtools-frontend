55780d66c8386969af40cbca3f1d4e7af3a3f7ee
Silica <earsyum@gmail.com>
2017-09-12 13:05:44 +1200

    Add files via upload

--- easyinstallers/PSMRuntime/main.py
+++ easyinstallers/PSMRuntime/main.py
@@ -4,12 +4,13 @@ import shutil
 import tkMessageBox
 
 import defs
+from easyinstallers.PSMRuntime import signTo
+
 
 def run():
     CMA = defs.getCmaDir()
     if not os.path.exists(CMA + "/EXTRACTED/APP/PCSI00011"):
         shutil.copytree(defs.getWorkingDir() + "/easyinstallers/PSMRuntime/PCSI00011", CMA + "/EXTRACTED/APP/PCSI00011")
-    import easyinstallers.PSMRuntime.signTo as signTo
     signTo.vp_start_gui()
 
 
